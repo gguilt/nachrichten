eb6f65e9bf40233a8eed1e72936bbd739d7fdc9b
gguilt <58526116+gguilt@users.noreply.github.com>
2022-12-17 17:35:45 +0300

    Removed unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,17 @@
-import React, { useState, useEffect } from "react";
-
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
-import MainHeader from "./components/MainHeader/MainHeader";
-
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [items, setItems] = useState([""]);
 
-  useEffect(() => {
-    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
-
-    if (storedUserLoggedInInformation === "1") {
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  const loginHandler = (email: any, password: string) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem("isLoggedIn", "1");
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem("isLoggedIn");
-    setIsLoggedIn(false);
-  };
+  setItems(["Merhaba Dünya", "Test"]);
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
+    <>
+      {items.map((e) => {
+        return <>{items[0]}</>
+      })}
+    </>
   );
 }
 
